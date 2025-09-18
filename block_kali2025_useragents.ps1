44d6b4812532758cc793bd6c4535e51c5dc96217
brandsict-nl <wiebren@brandsict.nl>
2025-09-18 14:19:15 +0200

    Add files via upload

---
+++ block_kali2025_useragents.ps1
@@ -0,0 +1,45 @@
+Import-Module WebAdministration
+
+$ruleName = "kali tools 2025"
+
+# Add a request filtering rule to deny specific User-Agent strings server-wide
+Add-WebConfigurationProperty -PSPath "IIS:\" `
+    -Filter "system.webServer/security/requestFiltering/filteringRules" `
+    -Name "." `
+    -Value @{
+        name = $ruleName
+        denyStrings = @("bot", "crawler")  # Add User-Agent strings to block
+    }
+
+# Configure the rule to apply to the User-Agent header
+Add-WebConfigurationProperty -PSPath "IIS:\" `
+    -Filter "system.webServer/security/requestFiltering/filteringRules/filteringRule[@name='$ruleName']/scanHeaders" `
+    -Name "." `
+    -Value "User-Agent"
+
+# Add denyStrings to the  filtering rule
+
+
+# dirbuster 1.0 rc1 
+# useragent: DirBuster-1.0-RC1+(http://www.owasp.org/index.php/Category:OWASP_DirBuster_Project)
+Add-WebConfigurationProperty -PSPath "IIS:\" `
+    -Filter "system.webServer/security/requestFiltering/filteringRules/filteringRule[@name='$ruleName']/denyStrings" `
+    -Name "." `
+    -Value @{string="DirBuster"} `
+    -Force
+
+# fuff 2.1.0 dev
+# useragent: Fuzz+Faster+U+Fool+v2.1.0-dev 
+Add-WebConfigurationProperty -PSPath "IIS:\" `
+    -Filter "system.webServer/security/requestFiltering/filteringRules/filteringRule[@name='$ruleName']/denyStrings" `
+    -Name "." `
+    -Value @{string="Fuzz"} `
+    -Force
+	
+# gobuster
+# useragent: gobuster/3.6
+Add-WebConfigurationProperty -PSPath "IIS:\" `
+    -Filter "system.webServer/security/requestFiltering/filteringRules/filteringRule[@name='$ruleName']/denyStrings" `
+    -Name "." `
+    -Value @{string="gobuster"} `
+    -Force
